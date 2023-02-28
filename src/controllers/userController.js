e6214c5608b5ea3d65f6248f5eab8ed48e7c436c
= <=>
2023-02-28 22:26:29 +0700

    fix mistake api/allcode

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -75,7 +75,6 @@ let handleDeleteUser = async (req, res) => {
 let getAllCode = async (req, res) => {
     try {
         let data = await userService.getAllCodeService(req.query.type);
-        console.log(data);
         return res.status(200).json(data);
     } catch (error) {
         console.log(error);
