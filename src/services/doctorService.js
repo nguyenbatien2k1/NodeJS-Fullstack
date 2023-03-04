a765ad24bdbd4b2626138893f919ab997aab4ca3
= <=>
2023-03-04 16:21:56 +0700

    change api createInfoDoctor

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -67,26 +67,63 @@ let getAllDoctors = () => {
 let createInfoDoctor = (data) => {
     return new Promise( async (resolve, reject) => {
         try {
-            if(!data.doctorId || !data.contentHTML || !data.contentMarkdown) {
+            if(!data.doctorId || !data.contentHTML || !data.contentMarkdown || !data.action) {
                 resolve({
                     errCode: 1,
                     errMessage: "Missing parameter..."
                 })
             }
             else {
-                await db.Markdown.create({
-                    contentHTML: data.contentHTML,
-                    contentMarkdown: data.contentMarkdown,
-                    description: data.description,
-                    doctorId: data.doctorId,
-                    specialtyId: data.specialtyId,
-                    clinicId: data.clinicId
-                })
+                if(data.action === 'CREATE') {
+                    let user = await db.Markdown.findOne({
+                        where: {doctorId: data.doctorId}
+                    })
+                    if(!user) {
+                        await db.Markdown.create({
+                            contentHTML: data.contentHTML,
+                            contentMarkdown: data.contentMarkdown,
+                            description: data.description,
+                            doctorId: data.doctorId
+                        })
+        
+                        resolve({
+                            errCode: 0,
+                            errMessage: 'Create doctor info success!'
+                        })
+                    }
+                    else {
+                        resolve({
+                            errCode: -1,
+                            errMessage: 'Create doctor info failed!'
+                        })
+                    }
+                }
+                else {
+                    let user = await db.Markdown.findOne({
+                        where: {doctorId: data.doctorId}
+                    })
+                    if(user) {
+                        await db.Markdown.update(
+                        {
+                            contentHTML: data.contentHTML,
+                            contentMarkdown: data.contentMarkdown,
+                            description: data.description,
+                            doctorId: data.doctorId
+                        }, 
+                        {
+                            where: {doctorId: data.doctorId}
+                        })
 
-                resolve({
-                    errCode: 0,
-                    errMessage: 'Create doctor info success!'
-                })
+                        resolve({
+                            errCode: 0,
+                            errMessage: 'Update doctor info success!'
+                        })
+                    }
+                    else resolve({
+                        errCode: 1,
+                        errMessage: `User isn't exist in the system...`
+                    })
+                }
             }
         } catch (error) {
             reject(error);
@@ -109,15 +146,24 @@ let getDetailDoctor = (doctorId) => {
                         id: doctorId
                     },
                     attributes: {
-                        exclude: ['password', 'image']
+                        exclude: ['password']
                     },
                     include: [
                         {model: db.Markdown, attributes: ['description', 'contentHTML', 'contentMarkdown']},
                         {model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi']},
                     ],
-                    raw: true,
+                    raw: false,
                     nest: true,
                 })
+                if(data) {
+                    if(data.image) {
+                        data.image = new Buffer(data.image, 'base64').toString('binary');
+                    }
+                }
+                else {
+                    data = {}
+                }
+
                 resolve({
                     errCode: 0,
                     data: data
